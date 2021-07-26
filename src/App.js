002d61e14e6a9b18e27437b698810998ab6e3238
janlay <janlay884181317@gmail.com>
2021-07-26 23:01:05 +0800

    feat: 完成第一版高亮

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import {CodeHighlight} from "./CodeHighlight";
 
 
 function App() {
-    const source = `ArrayBuffer.isView()`;
+    const source = `
+    const element = document.getElementById('some-element-you-want-to-animate');
+let start;
+
+function step(timestamp) {
+  if (start === undefined)
+    start = timestamp;
+  const elapsed = timestamp - start;
+
+  element.style.transform = 'translateX(' + Math.min(0.1 * elapsed, 200) + 'px)';
+
+  if (elapsed < 2000) { // 在两秒后停止动画
+    window.requestAnimationFrame(step);
+  }
+}
+
+window.requestAnimationFrame(step); 
+    `;
 
     return (
     <div className="App">
--- src/CodeHighlight/index.tsx
+++ src/CodeHighlight/index.tsx
@@ -1,56 +1,66 @@
 import * as React from "react";
 import {refractor} from "refractor";
 import {last, omit} from "lodash";
+import classnames from 'classnames';
+import './index.css'
 
 interface Props {
     lang: string,
     source: string
 }
 
-export const clone = path => path.map((node) => ({...node}));
+export const clone = (path: Record<any, any>[]) => path.map((node) => ({...node}));
 
-export const replace = (path, leaf) => [...clone(path.slice(0, -1)), leaf];
+export const replace = (path: any, leaf: any) => [...clone(path.slice(0, -1)), leaf];
 
-const treeToPathList = (node, output = [], path = []) => {
+const treeToPathList = (node: Record<string, any>, output = [], path = []) => {
     const nodeToUse = omit(node, 'children');
 
     if (node.children) {
+        // @ts-ignore
         path.push(nodeToUse);
         for (const child of node.children) {
             treeToPathList(child, output, path);
         }
         path.pop();
-    }
-    else {
+    } else {
+        // @ts-ignore
         output.push(clone([...path.slice(1), nodeToUse]));
     }
 
     return output;
 };
-
+// @ts-ignore
 const splitPathToLines = path => {
     const leaf = last(path);
+    // @ts-ignore
     if (!leaf.value.includes('\n')) {
         return [path];
     }
 
+    // @ts-ignore
     const linesOfText = leaf.value.split('\n');
+    // @ts-ignore
     return linesOfText.map(line => replace(path, {...leaf, value: line}));
 };
 
+// @ts-ignore
 const splitByLineBreak = paths => paths.reduce(
+    // @ts-ignore
     (lines, path) => {
         const currentLine = last(lines);
         const [currentRemaining, ...nextLines] = splitPathToLines(path);
         return [
             ...lines.slice(0, -1),
+            // @ts-ignore
             [...currentLine, currentRemaining],
+            // @ts-ignore
             ...nextLines.map(path => [path]),
         ];
     },
     [[]]
 );
-
+// @ts-ignore
 const toTokenTree = tree => {
     const paths = treeToPathList(tree);
     const linesOfPaths = splitByLineBreak(paths);
@@ -61,13 +71,16 @@ export const CodeHighlight: React.FC<Props> = (props) => {
     const {lang, source} = props;
 
     const tokenTree = toTokenTree(refractor.highlight(source, lang));
-
+// @ts-ignore
     const td = (item) => {
+        // @ts-ignore
         return item.map(token => {
+            console.log(token);
             if (token[0].type === 'text') {
                 return <span>{token[0].value}</span>
             } else {
-                return <span>{token[1].value}</span>
+                console.log(token[0]);
+                return <span className={classnames(...token[0].properties.className)}>{token[1].value}</span>
             }
         })
     }
@@ -77,12 +90,15 @@ export const CodeHighlight: React.FC<Props> = (props) => {
             <table>
                 <tbody>
                 {
+                    // @ts-ignore
                     tokenTree.map((item, index) => {
-                        return (<tr key={index}>
-                            <td style={{whiteSpace: 'pre-wrap'}}>
-                                {td(item)}
-                            </td>
-                        </tr>)
+                        return (
+                            <tr key={index}>
+                                <td style={{whiteSpace: 'pre-wrap'}}>
+                                    {td(item)}
+                                </td>
+                            </tr>
+                        )
                     })
                 }
                 </tbody>
